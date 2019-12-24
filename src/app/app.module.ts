7322717d92850f03e346a9480c4cc98644b69cc1
Wenjie Zhong <wzhong@procentec.com>
2019-12-24 22:53:43 +0100

    Add masonry images to certificates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material";
+import { MdcImageListModule } from "@angular-mdc/web";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -20,7 +21,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MdcImageListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "app-certificates",
   templateUrl: "./certificates.component.html",
-  styleUrls: ["./certificates.component.css"]
+  styleUrls: ["./certificates.component.scss"]
 })
 export class CertificatesComponent implements OnInit {
-  cpp = "assets/AT Computing C++.jpg";
-  cognitiveComputing = "assets/Cognitive Computing.png";
-  businessCourse = "assets/Ohio Business Course.jpg";
-  tmap = "assets/TMap.jpg";
+  certificateImages = [];
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.certificateImages = [
+      {
+        image: "assets/AT Computing C++.jpg",
+        description: "AT Computing C++"
+      },
+      {
+        image: "assets/Cognitive Computing.png",
+        description: "Cognitive Computing"
+      },
+      {
+        image: "assets/Ohio Business Course.jpg",
+        description: "Business Course"
+      },
+      {
+        image: "assets/Introduction to Swift Programming.pdf",
+        description: "Introduction to Swift Programming"
+      },
+      {
+        image: "assets/TMap.jpg",
+        description: "TMap"
+      },
+      {
+        image: "assets/iOS App Development Basics.pdf",
+        description: "iOS App Development Basics"
+      },
+      {
+        image: "assets/App Design and Development for iOS.pdf",
+        description: "App Design and Development for iOS"
+      }
+    ];
+  }
+
+  openFullCertificateImage() {
+    console.log("stuff");
+  }
 }
