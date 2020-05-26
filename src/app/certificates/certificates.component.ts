da8da3a0292f451042cb9fa95541b55ed3cda790
Wensh <wenjiezhong@me.com>
2020-05-26 15:36:05 +0200

    Add Bootstrap course

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 @Component({
   selector: "app-certificates",
   templateUrl: "./certificates.component.html",
-  styleUrls: ["./certificates.component.scss"]
+  styleUrls: ["./certificates.component.scss"],
 })
 export class CertificatesComponent implements OnInit {
   certificateImages = [];
@@ -13,48 +13,52 @@ export class CertificatesComponent implements OnInit {
     this.certificateImages = [
       {
         image: "assets/at_computing_cpp.jpg",
-        description: "AT Computing C++"
+        description: "AT Computing C++",
       },
       {
         image: "assets/cognitive_computing.png",
-        description: "Cognitive Computing"
+        description: "Cognitive Computing",
       },
       {
         image: "assets/ohio_business_course.jpg",
-        description: "Business Course"
+        description: "Business Course",
       },
       {
         image: "assets/introduction_to_swift_programming.jpg",
-        description: "Introduction to Swift Programming"
+        description: "Introduction to Swift Programming",
       },
       {
         image: "assets/objective_c_for_beginners.jpg",
-        description: "Objective C for beginners"
+        description: "Objective C for beginners",
       },
       {
         image: "assets/c_programming_ios_development.jpg",
-        description: "C Programming iOS Development Starts Here"
+        description: "C Programming iOS Development Starts Here",
       },
       {
         image: "assets/tmap.jpg",
-        description: "TMap"
+        description: "TMap",
       },
       {
         image: "assets/ios_app_development_basics.jpg",
-        description: "iOS App Development Basics"
+        description: "iOS App Development Basics",
       },
       {
         image: "assets/app_design_and_development_for_ios.jpg",
-        description: "App Design and Development for iOS"
+        description: "App Design and Development for iOS",
       },
       {
         image: "assets/profibus.jpg",
-        description: "Certified PROFIBUS Engineer"
+        description: "Certified PROFIBUS Engineer",
       },
       {
         image: "assets/angular_the_complete_guide.jpg",
-        description: "Angular - The Complete Guide"
-      }
+        description: "Angular - The Complete Guide",
+      },
+      {
+        image: "assets/bootstrap.jpg",
+        description: "Bootstrap 4",
+      },
     ];
   }
 }
