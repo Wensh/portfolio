ba1f7283cca1f015476651bce890929030f1fd22
Wenjie Zhong <wenjiezhong@me.com>
2023-04-05 14:23:21 +0200

    cli to 11

--- src/app/certificate-detail/certificate-detail.component.spec.ts
+++ src/app/certificate-detail/certificate-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 
 import { CertificateDetailComponent } from "./certificate-detail.component";
 
@@ -6,7 +6,7 @@ describe("CertificateDetailComponent", () => {
   let component: CertificateDetailComponent;
   let fixture: ComponentFixture<CertificateDetailComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [CertificateDetailComponent]
     }).compileComponents();
--- src/app/menu-overview/menu-overview.component.spec.ts
+++ src/app/menu-overview/menu-overview.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 
 import { MenuOverviewComponent } from "./menu-overview.component";
 
@@ -6,7 +6,7 @@ describe("MenuOverviewComponent", () => {
   let component: MenuOverviewComponent;
   let fixture: ComponentFixture<MenuOverviewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [MenuOverviewComponent]
     }).compileComponents();
