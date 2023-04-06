50497db3fb65a594d78f6be00e64849990e0002d
Wenjie Zhong <wenjiezhong@me.com>
2023-04-06 02:59:14 +0200

    add native masonry to portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { MatButtonModule } from "@angular/material/button";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { MenuOverviewComponent } from './menu-overview/menu-overview.component';
-import { CertificatesComponent } from './certificates/certificates.component';
-import { CertificateDetailComponent } from './certificate-detail/certificate-detail.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { MenuOverviewComponent } from "./menu-overview/menu-overview.component";
+import { CertificatesComponent } from "./certificates/certificates.component";
+import { CertificateDetailComponent } from "./certificate-detail/certificate-detail.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MasonryPipe } from "./common/masonry.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    MenuOverviewComponent,
-    CertificatesComponent,
-    CertificateDetailComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-  ],
+  declarations: [AppComponent, HomeComponent, MenuOverviewComponent, CertificatesComponent, CertificateDetailComponent, MasonryPipe],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
